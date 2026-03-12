04b3492485492bcef70e672b35cf5b522011f3e5
Julia Schohl <schohl.julia@gmail.com>
2026-03-11 20:57:21 -0400

    Clamp subpolar shoulder against subtropical bleed

--- src/main/java/com/example/globe/world/LatitudeBiomes.java
+++ src/main/java/com/example/globe/world/LatitudeBiomes.java
@@ -1659,6 +1659,9 @@ public final class LatitudeBiomes {
         double blendNoise = blobNoise01ScaledBlocks(WORLD_SEED, blockX, blockZ, blendPatchBlocks, BLEND_NOISE_SALT);
 
         int chosenBandIndex = blendNoise < blendT ? upperBandIndex : lowerBandIndex;
+        if (latNorm >= (LatitudeBands.Band.SUBPOLAR.lowDeg() / 90.0)) {
+            chosenBandIndex = Math.max(chosenBandIndex, BAND_SUBPOLAR);
+        }
 
         if (DEBUG_BLEND
                 && (blockX & 15) == 0
